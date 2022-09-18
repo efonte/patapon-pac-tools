6b42718ec61185e4d518b9a7c4c378060f9f20a2
efonte <3957463+efonte@users.noreply.github.com>
2022-09-18 02:30:28 +0200

    Update set_inst_names.py

--- set_inst_names.py
+++ set_inst_names.py
@@ -1,9 +1,6 @@
-import os
-from pathlib import Path
-from struct import unpack
-from typing import Dict, List, Tuple
-from rich import print
+import collections
 
+from rich import print
 
 functions = {}
 # with open("[P3] - PAC Instruction Dump.txt", "r", encoding="utf-8") as infile:
@@ -16,6 +13,13 @@ with open("[P2] - PAC Instruction Dump.txt", "r", encoding="utf-8") as infile:
         # print(f"{category:02X} {id:04X} {func_name}")
         functions[f"{category}_{id:04X}"] = (category, id, func_name)
 
+func_names = []
+for key, value in functions.items():
+    func_names.append(value[2])
+duplicated_func_names = [
+    item for item, count in collections.Counter(func_names).items() if count > 1
+]
+
 # outfile = open("p3_instruction_set_new.csv", "w", encoding="utf-8")
 # with open("p3_instruction_set.csv", "r", encoding="utf-8") as infile:
 outfile = open("p2_instruction_set_new.csv", "w", encoding="utf-8")
@@ -26,4 +30,6 @@ with open("p2_instruction_set.csv", "r", encoding="utf-8") as infile:
             _, _, func_name_new = functions[f"{category}_{id}"]
         except KeyError:
             func_name_new = func_name
+        if func_name_new in duplicated_func_names:
+            func_name_new = f"{func_name_new}{category}"
         outfile.write(f"{category};{id};{func_name_new};{desc};{params}\n")
